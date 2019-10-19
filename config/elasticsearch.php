3ba2ca963ba394613947ac69a9812aed3e0ebeb4
iGEM-2019-UESTC-Software <2017090402010@std.uestc.edu.cn>
2019-10-19 13:02:41 +0800

    config

---
+++ config/elasticsearch.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'host' => env('ELASTICSEARCH_HOST'),
+    'indices' => [
+        'mappings' => [
+            'default' => [
+                '_doc' => [
+                    'properties' => [
+                        'id' => [
+                            'type' => 'keyword',
+                        ],
+                    ],
+                ],
+            ],
+        ],
+        'settings' => [
+            'default' => [
+                'number_of_shards' => 1,
+                'number_of_replicas' => 0,
+            ],
+        ],
+    ],
+];
---
+++ config/filesystems.php
@@ -0,0 +1,79 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Filesystem Disk
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the default filesystem disk that should be used
+    | by the framework. The "local" disk, as well as a variety of cloud
+    | based disks are available to your application. Just store away!
+    |
+    */
+
+    'default' => env('FILESYSTEM_DRIVER', 's3'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Cloud Filesystem Disk
+    |--------------------------------------------------------------------------
+    |
+    | Many applications store files both locally and in the cloud. For this
+    | reason, you may specify a default "cloud" driver here. This driver
+    | will be bound as the Cloud disk implementation in the container.
+    |
+    */
+
+    'cloud' => env('FILESYSTEM_CLOUD', 's3'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Filesystem Disks
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure as many filesystem "disks" as you wish, and you
+    | may even configure multiple disks of the same driver. Defaults have
+    | been setup for each driver as an example of the required options.
+    |
+    | Supported Drivers: "local", "ftp", "sftp", "s3"
+    |
+    */
+
+    'disks' => [
+
+        'local' => [
+            'driver' => 'local',
+            'root' => storage_path('app'),
+            'permissions' => [
+                'file' => [
+                    'public' => 0666,
+                    'private' => 0600,
+                ],
+                'dir' => [
+                    'public' => 0775,
+                    'private' => 0700,
+                ],
+            ],
+        ],
+
+        'public' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+        ],
+
+        's3' => [
+            'driver' => 's3',
+            'key' => 'AKIAIRSJBROZWR24M42A',
+            'secret' => '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',
+            'region' => 'us-east-2',
+            'bucket' => 'igem123',
+            'url' => 'https://biomaster.s3-us-east-2.amazonaws.com/',
+        ],
+
+    ],
+
+];
